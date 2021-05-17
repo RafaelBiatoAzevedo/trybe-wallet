89741634e36fb3f3f7fac0e5d72d4d2580f1890e
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-05-17 11:43:32 -0300

    Updates

---
+++ precache-manifest.07a30edfce1f1602980e09e7ffa8e7ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53052522f7b85182aaaa68d04db5dc28",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "f834af4377cbd812e072",
+    "url": "/trybe-wallet/static/css/main.65c57e2c.chunk.css"
+  },
+  {
+    "revision": "ae351aa077155232c1b6",
+    "url": "/trybe-wallet/static/js/2.fa86e5be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet/static/js/2.fa86e5be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f834af4377cbd812e072",
+    "url": "/trybe-wallet/static/js/main.60f0df42.chunk.js"
+  },
+  {
+    "revision": "59d3c1ec903278203d08",
+    "url": "/trybe-wallet/static/js/runtime-main.2e2bea36.js"
+  },
+  {
+    "revision": "2c188a3ce286cf31088a61f7316a2c4b",
+    "url": "/trybe-wallet/static/media/enchange.2c188a3c.jpg"
+  }
+]);
